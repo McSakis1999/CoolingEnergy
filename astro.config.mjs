ee65d8035bfb298601ee0b7b62baa591501f192a
Mitsikostas <58091482+McSakis1999@users.noreply.github.com>
2026-07-21 17:33:39 +0300

    github actions added

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,13 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-
-import tailwindcss from '@tailwindcss/vite';
+import { defineConfig } from "astro/config";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://mcsakis1999.github.io",
+  base: "/CoolingEnergy",
+
   vite: {
-    plugins: [tailwindcss()]
-  }
+    plugins: [tailwindcss()],
+  },
 });
